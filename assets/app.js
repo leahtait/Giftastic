285ca4b1dae7a47dedadf0a5d7c4ef7ce87c3eeb
Leah Tait <leah.tait@gmail.com>
2019-05-04 17:30:01 -0700

    fixing some final details

--- assets/app.js
+++ assets/app.js
@@ -1,26 +1,11 @@
-// <!-- when new buttons made, they can be stuck in the below div -->
-// <div id="buttons-view"></div>
-
-// <form id="hero-form">
-//         <label for="hero-input">Type the superhero you want to invite to our gif party: </label>
-//         <input type="text" id="hero-input">
-//         <br>
-
-//         <!-- button to trigger generation of new button -->
-//         <input id="add-hero" type="submit" value="MORE SUPERHEROES!">
-//       </form>
-
-//       <!-- div to put in new gifs-->
-//       <div id="hero-view"></div>
-
 //initial array of buttons
 var superheroes = ["Thor", "Iron Man", "Wonder Woman", "Shazam!", "Superman", "Green Lantern", "Black Widow", "The Hulk"]
-// example URL "https://api.giphy.com/v1/gifs/random?api_key=8QKb7YZpKeFZKZSDMxiOKbY2Ofb9J7kT&tag=thor";
 
+//making a function to display the GIF
 function displayHeroGif() {
 
   var hero = $(this).attr("data-name");
-  var queryURL = "https://api.giphy.com/v1/gifs/random?" + "api_key=8QKb7YZpKeFZKZSDMxiOKbY2Ofb9J7kT" + "&tag=" + hero;
+  var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=8QKb7YZpKeFZKZSDMxiOKbY2Ofb9J7kT&tag=" + hero;
 
   // Creating an AJAX call for the specific hero button being clicked
   $.ajax({
@@ -28,15 +13,17 @@ function displayHeroGif() {
     method: "GET"
   }).then(function (response) {
 
-    console.log(response.data)
-    // Creating a div to hold the hero 
+    console.log(response.data) //console.log to see what I'm getting from the API
+
+    // Creating a div to hold the hero gif
     var heroDiv = $("<div class='hero'>");
 
-    // Retrieving the URL for the image
+    // Retrieving the URL for the image 
     var imgURL = response.data.images.original.url;
 
     // Creating an element to hold the image
     var image = $("<img class='herogif'>").attr("src", imgURL);
+    //giving the image the attribute of the still/non-gif version from the giphy API. not using it here when we first load the image, just putting it there for elater
     image.attr("otherURL", response.data.images.original_still.url);
 
 
@@ -55,7 +42,7 @@ function renderButtons() {
   // Deleting old buttons prior to adding new buttons to avoid repeat buttons
   $("#buttons-view").empty();
 
-  // Looping through the array of movies
+  // Looping through the array of heroes
   for (var i = 0; i < superheroes.length; i++) {
 
     //generating buttons for each hero in the array
@@ -77,19 +64,20 @@ $("#add-hero").on("click", function (event) {
   // This line grabs the input from the textbox
   var hero = $("#hero-input").val().trim();
 
-  // Adding movie from the textbox to our array
+  // Adding hero from the textbox to our array
   superheroes.push(hero);
 
-  // Calling renderButtons which handles the processing of our movie array
+  // Calling renderButtons which handles the processing of our hero array
   renderButtons();
 });
 
-// Adding a click event listener to all elements with a class of "movie-btn"
+//click event listener for all elements with a class of "hero-btn". when you click hero-btn, run the displayHeroGif function
 $(document).on("click", ".hero-btn", displayHeroGif);
 
-// Calling the renderButtons function to display the intial buttons
+// Calling the renderButtons function to display the intial buttons. 
 renderButtons();
 
+//click event that when you click on a herogif, it changes the attribute on the image you click from it's current attribute to the "other" one. 
 $(document).on("click", ".herogif", function () {
   console.log("click");
   var src = $(this).attr("src");
